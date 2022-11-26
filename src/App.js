9bd7fa1287c1e735627b1d638f8cd4b1ff202fa7
David Domínguez Blanco <60604817+DavidDominguez2@users.noreply.github.com>
2022-11-26 09:26:53 +0100

    react-i18next

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+import { Suspense } from "react";
+
+function Welcome() {
+    const { t, i18n } = useTranslation(["welcome"]);
+    function changeLanguageToEnglish() {
+        i18n.changeLanguage("en");
+    }
+    function changeLanguageToSpanish() {
+        i18n.changeLanguage("es");
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p
+                    dangerouslySetInnerHTML={{
+                        __html: t("title", { name: "i18Next" }),
+                    }}
+                />
+                <p>Idioma actual: {i18n.language} </p>
+                <button onClick={changeLanguageToEnglish}>INGLES</button>
+                <button onClick={changeLanguageToSpanish}>ESPAÑOL</button>
+            </header>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Suspense fallback="Cargando traducciones...">
+            <Welcome />
+        </Suspense>
+    );
 }
 
 export default App;
